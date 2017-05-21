21f43126266077c7ceaaad07d0839d307dcc21f8
mrlisdim <mrlisdim@yandex.ru>
2017-05-22 00:44:02 +0300

    [IMPR] Added test fom a number of graphs

--- test.m
+++ test.m
@@ -1,4 +1,8 @@
 x = (0:1:10)';
-y = x.^2;
+y1 = x.^2;
+y2 = x.^3;
+y3 = x.^4;
 
-buildGraph([x y], 'x label — t', 'y label — \theta', [0 10, 0 100], 1);
+buildGraph([x y1], 'x label — t', 'y label — \theta', [0 10, 0 100], 1);
+buildGraph([x y2], 'x label — t', 'y label — \alpha', [0 10, 0 100], 0);
+buildGraph([x y3], 'x label — t', 'y label — \gamma', [0 10, 0 100], 0);
